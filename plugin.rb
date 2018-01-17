b63032a31687489fe7f5b6859381278662a976cf
Angus McLeod <angus@mcleod.org.au>
2018-01-17 16:11:27 +0800

    Add apps about category

--- plugin.rb
+++ plugin.rb
@@ -45,7 +45,29 @@ DiscourseEvent.on(:civically_site_ready) do
                                  'rating_enabled': true,
                                  'topic_types': 'rating'
                                })
-    SiteSetting.app_category_id = category.id
+
+    if category.save
+      SiteSetting.app_category_id = category.id
+
+      t = Topic.new(
+        title: I18n.t("about_apps.title"),
+        user: Discourse.system_user,
+        pinned_at: Time.now,
+        category_id: category.id
+      )
+      t.skip_callbacks = true
+      t.ignore_category_auto_close = true
+      t.delete_topic_timer(TopicTimer.types[:close])
+      t.save!(validate: false)
+
+      category.topic_id = t.id
+      category.save!
+
+      t.posts.create(
+        raw: I18n.t('about_apps.body'),
+        user: Discourse.system_user
+      )
+    end
   end
 end
 
